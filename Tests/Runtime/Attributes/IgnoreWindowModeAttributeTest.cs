f1dcfe067a21934157d1dd934c8d2af1b1dbeb34
Koji Hasegawa <hasegawa@hubsys.co.jp>
2023-10-22 01:25:58 +0900

    Add test on attributes attached to async Test and UnityTest

--- Tests/Runtime/Attributes/IgnoreWindowModeAttributeTest.cs
+++ Tests/Runtime/Attributes/IgnoreWindowModeAttributeTest.cs
@@ -1,8 +1,11 @@
 // Copyright (c) 2023 Koji Hasegawa.
 // This software is released under the MIT License.
 
+using System.Collections;
+using System.Threading.Tasks;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.TestTools;
 
 namespace TestHelper.Attributes
 {
@@ -11,8 +14,24 @@ namespace TestHelper.Attributes
     {
         [Test]
         [IgnoreWindowMode("Test for skip run on window-mode")]
-        public void AttachToMethod_SkipOnWindowMode()
+        public void Attach_SkipOnWindowMode()
         {
+            Assert.That(Application.isBatchMode, Is.True);
+        }
+
+        [Test]
+        [IgnoreWindowMode("Test for skip run on window-mode")]
+        public async Task AttachToAsyncTest_SkipOnWindowMode()
+        {
+            await Task.Yield();
+            Assert.That(Application.isBatchMode, Is.True);
+        }
+
+        [UnityTest]
+        [IgnoreWindowMode("Test for skip run on window-mode")]
+        public IEnumerator AttachToUnityTest_SkipOnWindowMode()
+        {
+            yield return null;
             Assert.That(Application.isBatchMode, Is.True);
         }
     }
