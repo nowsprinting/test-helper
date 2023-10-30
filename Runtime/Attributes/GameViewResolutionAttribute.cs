010c3393c1ab5875abec134c111387da7ff926a4
Koji Hasegawa <hasegawa@hubsys.co.jp>
2023-10-30 20:33:11 +0900

    Refactor GameViewResolutionAttribute

--- Runtime/Attributes/GameViewResolutionAttribute.cs
+++ Runtime/Attributes/GameViewResolutionAttribute.cs
@@ -5,11 +5,7 @@ using System;
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using NUnit.Framework.Internal;
-using TestHelper.RuntimeInternals.Wrappers.UnityEditor;
-using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
+using TestHelper.RuntimeInternals;
 
 namespace TestHelper.Attributes
 {
@@ -48,63 +44,7 @@ namespace TestHelper.Attributes
         /// <inheritdoc />
         public void ApplyToContext(ITestExecutionContext context)
         {
-#if UNITY_2022_2_OR_NEWER
-            SetResolutionUsingPlayModeWindow();
-#else
-            SetResolution();
-#endif
-        }
-
-        // ReSharper disable once UnusedMember.Local
-        private void SetResolutionUsingPlayModeWindow()
-        {
-#if UNITY_EDITOR && UNITY_2022_2_OR_NEWER
-            PlayModeWindow.SetViewType(PlayModeWindow.PlayModeViewTypes.GameView);
-            PlayModeWindow.SetCustomRenderingResolution(_width, _height, _name);
-#endif
-        }
-
-        // ReSharper disable once UnusedMember.Local
-        private void SetResolution()
-        {
-#if UNITY_EDITOR
-            var gameViewSizes = GameViewSizesWrapper.CreateInstance();
-            if (gameViewSizes == null)
-            {
-                Debug.LogError("GameViewSizes instance creation failed.");
-                return;
-            }
-
-            var gameViewSizeGroup = gameViewSizes.CurrentGroup();
-            if (gameViewSizeGroup == null)
-            {
-                Debug.LogError("GameViewSizeGroup instance creation failed.");
-                return;
-            }
-
-            var gameViewSize = GameViewSizeWrapper.CreateInstance((int)_width, (int)_height, _name);
-            if (gameViewSize == null)
-            {
-                Debug.LogError("GameViewSize instance creation failed.");
-                return;
-            }
-
-            var index = gameViewSizeGroup.IndexOf(gameViewSize);
-            if (index == -1)
-            {
-                gameViewSizeGroup.AddCustomSize(gameViewSize);
-                index = gameViewSizeGroup.IndexOf(gameViewSize);
-            }
-
-            var gameView = GameViewWrapper.GetWindow();
-            if (gameView == null)
-            {
-                Debug.LogError("GameView instance creation failed.");
-                return;
-            }
-
-            gameView.SelectedSizeIndex(index);
-#endif
+            GameViewControlHelper.SetResolution(_width, _height, _name);
         }
     }
 }
--- RuntimeInternals/GameViewControlHelper.cs
+++ RuntimeInternals/GameViewControlHelper.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Reflection;
+using TestHelper.RuntimeInternals.Wrappers.UnityEditor;
 using UnityEditor;
 using UnityEngine;
 
@@ -31,6 +32,73 @@ namespace TestHelper.RuntimeInternals
             }
 
             EditorWindow.GetWindow(s_gameView, false, null, true);
+#endif
+        }
+
+        /// <summary>
+        /// Set <c>GameView</c> resolution.
+        /// </summary>
+        /// <param name="width">GameView width [px]</param>
+        /// <param name="height">GameView height [px]</param>
+        /// <param name="name">GameViewSize name</param>
+        public static void SetResolution(uint width, uint height, string name)
+        {
+#if UNITY_2022_2_OR_NEWER
+            SetResolutionUsingPlayModeWindow(width, height, name);
+#else
+            SetResolutionUsingReflection(width, height, name);
+#endif
+        }
+
+        // ReSharper disable once UnusedMember.Local
+        private static void SetResolutionUsingPlayModeWindow(uint width, uint height, string name)
+        {
+#if UNITY_EDITOR && UNITY_2022_2_OR_NEWER
+            PlayModeWindow.SetViewType(PlayModeWindow.PlayModeViewTypes.GameView);
+            PlayModeWindow.SetCustomRenderingResolution(width, height, name);
+#endif
+        }
+
+        // ReSharper disable once UnusedMember.Local
+        private static void SetResolutionUsingReflection(uint width, uint height, string name)
+        {
+#if UNITY_EDITOR
+            var gameViewSizes = GameViewSizesWrapper.CreateInstance();
+            if (gameViewSizes == null)
+            {
+                Debug.LogError("GameViewSizes instance creation failed.");
+                return;
+            }
+
+            var gameViewSizeGroup = gameViewSizes.CurrentGroup();
+            if (gameViewSizeGroup == null)
+            {
+                Debug.LogError("GameViewSizeGroup instance creation failed.");
+                return;
+            }
+
+            var gameViewSize = GameViewSizeWrapper.CreateInstance((int)width, (int)height, name);
+            if (gameViewSize == null)
+            {
+                Debug.LogError("GameViewSize instance creation failed.");
+                return;
+            }
+
+            var index = gameViewSizeGroup.IndexOf(gameViewSize);
+            if (index == -1)
+            {
+                gameViewSizeGroup.AddCustomSize(gameViewSize);
+                index = gameViewSizeGroup.IndexOf(gameViewSize);
+            }
+
+            var gameView = GameViewWrapper.GetWindow();
+            if (gameView == null)
+            {
+                Debug.LogError("GameView instance creation failed.");
+                return;
+            }
+
+            gameView.SelectedSizeIndex(index);
 #endif
         }
     }
