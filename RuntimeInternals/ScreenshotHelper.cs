47371f276f1cae4691f677cd2243b9a41d02a5a6
Koji Hasegawa <hasegawa@hubsys.co.jp>
2023-11-01 10:53:48 +0900

    Make obsolete TestHelper.Utils.ScreenshotHelper class

--- RuntimeInternals/ScreenshotHelper.cs
+++ RuntimeInternals/ScreenshotHelper.cs
@@ -6,6 +6,9 @@ using System.IO;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using UnityEngine;
+#if UNITY_INCLUDE_TESTS
+using NUnit.Framework;
+#endif
 
 namespace TestHelper.RuntimeInternals
 {
@@ -20,9 +23,22 @@ namespace TestHelper.RuntimeInternals
             return Path.Combine(Application.persistentDataPath, "TestHelper", "Screenshots");
         }
 
+        private static string DefaultFilename(string callerMemberName)
+        {
+#if UNITY_INCLUDE_TESTS
+            return TestContext.CurrentTestExecutionContext.CurrentTest.Name
+                .Replace('(', '_')
+                .Replace(')', '_')
+                .Replace(',', '-');
+            // Note: Same as the file name created under ActualImages of the Graphics Tests Framework package.
+#else
+            return callerMemberName;
+#endif
+        }
+
         /// <summary>
         /// Take a screenshot and save it to file.
-        /// Default save path is $"{Application.persistentDataPath}/TestHelper/Screenshots/{CurrentTest.Name}.png".
+        /// Default save path is <c>Application.persistentDataPath</c> + "/TestHelper/Screenshots/" + <c>CurrentTest.Name</c> + ".png".
         /// </summary>
         /// <remarks>
         /// Limitations:
@@ -30,29 +46,27 @@ namespace TestHelper.RuntimeInternals
         ///  - Must be called from main thread.
         ///  - <c>GameView</c> must be visible. Use <c>FocusGameViewAttribute</c> or <c>GameViewResolutionAttribute</c> if running on batch mode.
         ///  - Files with the same name will be overwritten. Please specify filename argument when calling over twice in one method.
+        ///  - UniTask is required to be used from the async method. And also needs coroutineRunner (any MonoBehaviour) because TakeScreenshot method uses WaitForEndOfFrame inside. See more information: https://github.com/Cysharp/UniTask#ienumeratortounitask-limitation
         /// <br/>
         /// Using <c>ScreenCapture.CaptureScreenshotAsTexture</c> internally.
         /// </remarks>
-        /// <param name="directory">Directory to save screenshots.</param>
-        /// <param name="filename">Filename to store screenshot.</param>
+        /// <param name="directory">Directory to save screenshots. Default save path is <c>Application.persistentDataPath</c> + "/TestHelper/Screenshots/".</param>
+        /// <param name="filename">Filename to store screenshot.
+        /// Default filename is <c>CurrentTest.Name</c> + ".png" when run in test-framework context.
+        /// Using caller method name when run in runtime context.</param>
         /// <param name="superSize">The factor to increase resolution with.</param>
         /// <param name="stereoCaptureMode">The eye texture to capture when stereo rendering is enabled.</param>
         public static IEnumerator TakeScreenshot(
             string directory = null,
-            [CallerMemberName] string filename = null,
+            string filename = null,
             int superSize = 1,
-            ScreenCapture.StereoScreenCaptureMode stereoCaptureMode = ScreenCapture.StereoScreenCaptureMode.LeftEye
-        )
+            ScreenCapture.StereoScreenCaptureMode stereoCaptureMode = ScreenCapture.StereoScreenCaptureMode.LeftEye,
+            // ReSharper disable once InvalidXmlDocComment
+            [CallerMemberName] string callerMemberName = null)
         {
             if (superSize != 1 && stereoCaptureMode != ScreenCapture.StereoScreenCaptureMode.LeftEye)
             {
                 Debug.LogError("superSize and stereoCaptureMode cannot be specified at the same time.");
-                yield break;
-            }
-
-            if (filename == null)
-            {
-                Debug.LogError("filename must be specified.");
                 yield break;
             }
 
@@ -73,6 +87,11 @@ namespace TestHelper.RuntimeInternals
             }
 
             Directory.CreateDirectory(directory);
+
+            if (filename == null)
+            {
+                filename = DefaultFilename(callerMemberName);
+            }
 
             if (!filename.EndsWith(".png"))
             {
