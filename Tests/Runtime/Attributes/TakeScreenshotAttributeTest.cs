84b5e585289d5b41fb6ffdc922f6d811339f18d1
Koji Hasegawa <hasegawa@hubsys.co.jp>
2023-11-03 15:19:57 +0900

    Refactor TakeScreenshot tests

--- Tests/Runtime/Attributes/TakeScreenshotAttributeTest.cs
+++ Tests/Runtime/Attributes/TakeScreenshotAttributeTest.cs
@@ -16,6 +16,8 @@ namespace TestHelper.Attributes
     public class TakeScreenshotAttributeTest
     {
         private const string TestScene = "Packages/com.nowsprinting.test-helper/Tests/Scenes/ScreenshotTest.unity";
+        private const int FileSizeThreshold = 5441; // VGA size solid color file size
+        private const int FileSizeThreshold2X = 100 * 1024; // Normal size is 80 to 90KB
 
         private readonly string _defaultOutputDirectory =
             Path.Combine(Application.persistentDataPath, "TestHelper", "Screenshots");
@@ -57,6 +59,7 @@ namespace TestHelper.Attributes
                 _defaultOutputDirectory,
                 $"{nameof(Attach_SaveScreenshotToDefaultPath)}.png");
             Assert.That(path, Does.Exist);
+            Assert.That(File.ReadAllBytes(path), Has.Length.GreaterThan(FileSizeThreshold));
         }
 
         [Test, Order(0)]
@@ -85,6 +88,7 @@ namespace TestHelper.Attributes
                 _defaultOutputDirectory,
                 $"{nameof(AttachToAsyncTest_SaveScreenshotToDefaultPath)}.png");
             Assert.That(path, Does.Exist);
+            Assert.That(File.ReadAllBytes(path), Has.Length.GreaterThan(FileSizeThreshold));
         }
 
         [UnityTest, Order(0)]
@@ -112,6 +116,67 @@ namespace TestHelper.Attributes
             var path = Path.Combine(
                 _defaultOutputDirectory,
                 $"{nameof(AttachToUnityTest_SaveScreenshotToDefaultPath)}.png");
+            Assert.That(path, Does.Exist);
+            Assert.That(File.ReadAllBytes(path), Has.Length.GreaterThan(FileSizeThreshold));
+        }
+
+        [Test, Order(0)]
+        [LoadScene(TestScene)]
+        [TakeScreenshot]
+        public void AttachToParameterizedTest_SaveAllScreenshots(
+            [Values(0, 1)] int i,
+            [Values(2, 3)] int j)
+        {
+            var path = Path.Combine(
+                _defaultOutputDirectory,
+                $"{nameof(AttachToParameterizedTest_SaveAllScreenshots)}_{i}-{j}_.png");
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            Assume.That(path, Does.Not.Exist);
+
+            // Take screenshot after running the test.
+        }
+
+        [Test, Order(1)]
+        public void AttachToParameterizedTest_SaveAllScreenshots_ExistFile(
+            [Values(0, 1)] int i,
+            [Values(2, 3)] int j)
+        {
+            var path = Path.Combine(
+                _defaultOutputDirectory,
+                $"{nameof(AttachToParameterizedTest_SaveAllScreenshots)}_{i}-{j}_.png");
+            Assert.That(path, Does.Exist);
+        }
+
+        private const string SpecifyRelativeDirectory = "Logs/TestHelper/Screenshots";
+
+        [Test, Order(0)]
+        [LoadScene(TestScene)]
+        [TakeScreenshot(directory: SpecifyRelativeDirectory)]
+        public void AttachWithDirectory_SaveScreenshotToSpecifyPath()
+        {
+            var path = Path.Combine(
+                Path.GetFullPath(SpecifyRelativeDirectory),
+                $"{nameof(AttachWithDirectory_SaveScreenshotToSpecifyPath)}.png");
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            Assume.That(path, Does.Not.Exist);
+
+            // Take screenshot after running the test.
+        }
+
+        [Test, Order(1)]
+        public void AttachWithDirectory_SaveScreenshotToSpecifyPath_ExistFile()
+        {
+            var path = Path.Combine(
+                Path.GetFullPath(SpecifyRelativeDirectory),
+                $"{nameof(AttachWithDirectory_SaveScreenshotToSpecifyPath)}.png");
             Assert.That(path, Does.Exist);
         }
 
@@ -145,6 +210,64 @@ namespace TestHelper.Attributes
             Assert.That(path, Does.Exist);
         }
 
+        [Test, Order(0)]
+        [LoadScene(TestScene)]
+        [TakeScreenshot(superSize: 2)]
+        public void AttachWithSuperSize_SaveSuperSizeScreenshot()
+        {
+            var path = Path.Combine(
+                _defaultOutputDirectory,
+                $"{nameof(AttachWithSuperSize_SaveSuperSizeScreenshot)}.png");
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            Assume.That(path, Does.Not.Exist);
+
+            // Take screenshot after running the test.
+        }
+
+        [Test, Order(1)]
+        public void AttachWithSuperSize_SaveSuperSizeScreenshot_ExistFile()
+        {
+            var path = Path.Combine(
+                _defaultOutputDirectory,
+                $"{nameof(AttachWithSuperSize_SaveSuperSizeScreenshot)}.png");
+            Assert.That(path, Does.Exist);
+            Assert.That(File.ReadAllBytes(path), Has.Length.GreaterThan(FileSizeThreshold2X));
+        }
+
+        [Test, Order(0)]
+        [LoadScene(TestScene)]
+        [TakeScreenshot(stereoCaptureMode: ScreenCapture.StereoScreenCaptureMode.BothEyes)]
+        public void AttachWithStereoCaptureMode_SaveStereoScreenshot()
+        {
+            var path = Path.Combine(
+                _defaultOutputDirectory,
+                $"{nameof(AttachWithStereoCaptureMode_SaveStereoScreenshot)}.png");
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            Assume.That(path, Does.Not.Exist);
+
+            // Take screenshot after running the test.
+        }
+
+        [Test, Order(1)]
+        public void AttachWithStereoCaptureMode_SaveStereoScreenshot_ExistFile()
+        {
+            var path = Path.Combine(
+                _defaultOutputDirectory,
+                $"{nameof(AttachWithStereoCaptureMode_SaveStereoScreenshot)}.png");
+            Assert.That(path, Does.Exist);
+            // Is it a stereo screenshot? See for yourself! Be a witness!!
+            // Note: Require stereo rendering settings.
+            // See: https://docs.unity3d.com/Manual/SinglePassStereoRendering.html
+        }
+
         private class GizmoDemo : MonoBehaviour
         {
             private void OnDrawGizmos()
@@ -174,6 +297,7 @@ namespace TestHelper.Attributes
                 _defaultOutputDirectory,
                 $"{nameof(AttachWithGizmos_TakeScreenshotWithGizmos)}.png");
             Assert.That(path, Does.Exist);
+            // Is Gizmos shown in the screenshot? See for yourself! Be a witness!!
         }
     }
 }
